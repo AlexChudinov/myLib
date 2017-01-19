7a1281ab9aa78d9dd1b5990df7502cd946af5fa8
AlexChudinov <chudinov@inbox.ru>
2017-01-19 17:44:58 +0300

    Linear interpolation

---
+++ linearAlgebra/linearInterpolation.h
@@ -0,0 +1,79 @@
+#pragma once
+#ifndef _LINEAR_INTERPOLATION
+#define _LINEAR_INTERPOLATION
+
+#include "matrixTemplate.h"
+/**
+ * Linear interpolation procedures
+ */
+MATH_NAMESPACE_BEG
+
+/**
+ * Mono-dimensional interpolation
+ * Returns an interpolation coefs for an interpolation on a line
+ */
+template<typename Float>
+std::tuple<Float, Float> lineInterpolation(
+	const vector_c<Float, 3>& pos, 
+	const vector_c<Float, 3>& line_start,
+	const vector_c<Float, 3>& line_end
+)
+{
+	std::tuple<Float, Float> res;
+	vector_c<Float, 3>
+		line = line_end - line_start,
+		pos0 = pos - line_start;
+	Float t = pos0*line / sqr(line);
+	std::get<0>(res) = (1 - t);
+	std::get<1>(res) = t;
+	return res;
+}
+
+/**
+ * Two-dimensional interpolation
+ * Returns an interpolation coefs for an interpolation inside a triangle {x0 x1 x2}
+ */
+template<typename Float>
+std::tuple<Float, Float, Float> triInterpolation(
+	const vector_c<Float, 3>& pos,
+	const vector_c<Float, 3>& x0,
+	const vector_c<Float, 3>& x1,
+	const vector_c<Float, 3>& x2
+)
+{
+	std::tuple<Float, Float, Float> res;
+	vector_c<Float, 3>
+		pos0 = pos - x0,
+		xx1 = x1 - x0,
+		xx2 = x2 - x0;
+
+	//Create plane basis
+	vector_c<Float, 3>
+		e1 = xx1 / abs(xx1),
+		e2 = xx2 - (xx2*e1)*e1; e2 /= abs(e2);
+
+	//Shift to a plane basis
+	vector_c<Float, 2>
+		pos00{ e1*pos0, e2*pos0 },
+		xxx1{ e1*xx1, e2*xx1 },
+		xxx2{ e1*xx2, e2*xx2 };
+
+	//Solve linear equations
+	matrix_c<Float, 2, 2> eqns, mt1, mt2;
+	eqns.column(0) = xxx1 - xxx2; eqns.column(1) = pos00;
+	mt1.column(0) = xxx1; mt1.column(1) = eqns.column(1);
+	mt2.column(0) = eqns.column(0); mt2.column(1) = xxx1;
+
+	double fDet = det(eqns), t1 = det(mt1) / fDet, t2 = det(mt2) / fDet;
+
+	std::get<0>(res) = (1 - 1 / t2);
+	std::get<1>(res) = (1 - t1) / t2;
+	std::get<2>(res) = t1 / t2;
+
+	return res;
+}
+
+MATH_NAMESPACE_END
+
+#endif // !_LINEAR_INTERPOLATION
+
