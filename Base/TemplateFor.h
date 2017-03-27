4cfcdbda921ef70947c5fad67e3e3bad0f2fe506
AlexChudinov <chudinov@inbox.ru>
2017-03-27 16:17:10 +0300

    Some small updates

--- Base/TemplateFor.h
+++ Base/TemplateFor.h
@@ -14,18 +14,14 @@ template<size_t begin, size_t end, size_t step = 1>
 struct For
 {
 	using next = For<begin + step, end, step>;
-	
-#define DEF_CHECK_ENDS \
-	static_assert(begin < end, "Start value is bigger than the finish value in a template for loop.");
 
 	template<typename O, typename ... T>
 	static inline void Do(O Op, T ... args)
 	{
-		DEF_CHECK_ENDS
+		static_assert(begin < end, "Start value is bigger than the finish value in a template for loop.");
 		Op(begin, args...);
 		next::Do(Op, args...);
 	}
-#undef DEF_CHECK_ENDS
 };
 /**
  * If begin == end then stop the loop
